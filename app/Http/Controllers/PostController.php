4c859a3faa4ba6d6a7d29237669c5b1ba24ff904
renho <renhosoya19990807@gmail.com>
2025-03-07 17:21:00 +0900

    モデルの作成とコントローラの中身を修正する

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * @return
-     */
-    public function index(): View
+    public $post;
+
+    public function __construct()
     {
-        
+        $this->post = new Post(); // 投稿モデルをインスタンス化
+    }
+
+    public function index()
+    {
+        $data = $this->post->get_posts_data();
+        dd($data);
     }
 }
