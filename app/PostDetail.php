e663e889c2f6422cef995b3224e17651bd8feb0d
renho <renhosoya19990807@gmail.com>
2025-03-10 14:58:54 +0900

    投稿詳細のファイルを作成

---
+++ app/PostDetail.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostDetail extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostDetailFactory> */
+    use HasFactory;
+}
