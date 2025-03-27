d3d18ab7707be1a53515b20ead4e4875572aa8e3
renho <renhosoya19990807@gmail.com>
2025-03-27 16:54:47 +0900

    フロントエンドのエラーを修正

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
+        'auth' => [
+            'user' => Auth::user(),
+        ],
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -15,13 +20,36 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return Inertia::render('Dashboard',[
+        'auth' => [
+            'user' => Auth::user(),
+        ],
+    ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile', function () {
+        return Inertia::render('Profile/Edit', [
+            'auth' => [
+                'user' => Auth::user(),
+            ],
+        ]);
+    })->name('profile.edit');
+
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::get('/', [PostController::class, 'index'])->name('index');
+// Route::get('/create', [PostController::class, 'create'])->name('create');
+// Route::post('/store', [PostController::class, 'store'])->name('store');
+// Route::get('/show/{id}', [PostController::class, 'show'])->name('show');
+// Route::get('/edit/{id}', [PostController::class, 'edit'])->name('edit');
+// Route::put('/update', [PostController::class, 'update'])->name('update');
+// Route::delete('/delete', [PostController::class, 'destroy'])->name('delete');
+
+// Route::post('/ajax/eval',[PostController::class, 'eval'])->name('ajax/eval');
 
 require __DIR__.'/auth.php';
