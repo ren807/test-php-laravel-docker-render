83ae507a02c540962d19deeee18c26e31773b84c
renho <renhosoya19990807@gmail.com>
2025-03-27 13:20:19 +0900

    Laravel Breezeの導入まで

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,27 @@
 <?php
 
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/wel', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/', [PostController::class, 'index'])->name('index');
-Route::get('/create', [PostController::class, 'create'])->name('create');
-Route::post('/store', [PostController::class, 'store'])->name('store');
-Route::get('/show/{id}', [PostController::class, 'show'])->name('show');
-Route::get('/edit/{id}', [PostController::class, 'edit'])->name('edit');
-Route::put('/update', [PostController::class, 'update'])->name('update');
-Route::delete('/delete', [PostController::class, 'destroy'])->name('delete');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::post('/ajax/eval',[PostController::class, 'eval'])->name('ajax/eval');
+require __DIR__.'/auth.php';
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [
-		laravel([
-			'resources/css/app.css',
-			'resources/js/app.js',
-		]),
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: true,
+        }),
         react(),
-	],
+    ],
 });
